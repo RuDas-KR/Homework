2a24fb80b80d657ebd1b3048daadfad023e3e732
RuDas-KR <jjm1411@naver.com>
2014-10-14 21:20:17 +0900

    adjust second

--- test.c
+++ test.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void my_sort(int *a_array, int a_size, int *a_sort);
+void my_sort(int *a, int size);
 
 int main(void){
-	int a[] = {9, 3, 5, 7, 1};
-	int b[5];
-
-	my_sort(a, 5, b);
+   int a[] = {9, 3, 5, 7, 1};
+   my_sort(a, 5);
 }
 
-void my_sort(int *a_array, int a_size, int *a_sort)
+void my_sort(int *a, int size)
 {
-	int i, j, biggest;
-	int size = sizeof(a_array)/sizeof(a_array[0]);
-	
-	for(i=0; i<size-1; i++)
-		for(j=0; j<size; j++)
-		{
-			if(a_array[i] > a_array[j])
-			{
-				biggest = a_array[i];
-				a_array[i] = a_array[j];
-				a_array[j] = biggest;
-			}
-		}
-	printf("정렬 후 데이터\n");
-	for(i=0; i<size; i++)
-		printf("%4d", a_array[i]);
-	printf("\n");
+   int i, j, tmp;
+    for(i=0; i<size; i++)
+    {
+       for(j=i+1; j<size; j++)
+       {
+       	   if(a[i] > a[j])
+       	   {
+       	   	tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+       	   }
+       }
+    }
+   printf("정렬 후 데이터\n");
+   for(i=0; i<size; i++)
+      printf("%4d", a[i]);
+   printf("\n");
 }
