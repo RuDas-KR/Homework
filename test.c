e84c497b90e5f2ae167b4bfc0427241250db8d22
RuDas-KR <jjm1411@naver.com>
2014-10-14 20:42:36 +0900

    adjust first

--- test.c
+++ test.c
@@ -12,12 +12,21 @@ int main(void){
 
 void my_sort(int *a_array, int a_size, int *a_sort)
 {
-	int i, biggest;
-	biggest = a_array[0];
-
-	for(i=0; i<a_size; i++){
-		if(a_array[i]<biggest)
-			biggest = a_array[i];
-		i++;
-	}
+	int i, j, biggest;
+	int size = sizeof(a_array)/sizeof(a_array[0]);
+	
+	for(i=0; i<size-1; i++)
+		for(j=0; j<size; j++)
+		{
+			if(a_array[i] > a_array[j])
+			{
+				biggest = a_array[i];
+				a_array[i] = a_array[j];
+				a_array[j] = biggest;
+			}
+		}
+	printf("정렬 후 데이터\n");
+	for(i=0; i<size; i++)
+		printf("%4d", a_array[i]);
+	printf("\n");
 }
